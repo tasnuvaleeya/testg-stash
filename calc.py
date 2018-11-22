2c3531711f088b530597d0970e3c93768afa17c4
tasnuvaleeya <tasnuva2606@gmail.com>
2018-11-22 16:16:29 +0600

    initial commit

---
+++ calc.py
@@ -0,0 +1,20 @@
+def add(x, y):
+    """Add Function"""
+    pass
+
+
+def subtract(x, y):
+    """Subtract Function"""
+    pass
+
+
+def multiply(x, y):
+    """Multiply Function"""
+    pass
+
+
+def divide(x, y):
+    """Divide Function"""
+    # if y == 0:
+    #     raise ValueError('Can not divide by zero!')
+    pass
